08d03ea8643808fb27dd1dc3b5c7530033023dd7
Charles Averill <charlesaverill20@gmail.com>
2022-09-13 08:06:00 +0000

    Latest docs - 2022-09-10T00:42:04Z

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['append_5floaded_0',['append_loaded',['../llvm_8c.html#a93ed5095c63c5e965623499aa9572d33',1,'llvm.c']]],
-  ['ast_5fto_5fllvm_1',['ast_to_llvm',['../translate_8h.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c'],['../translate_8c.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c']]]
+  ['ast_5fto_5fllvm_0',['ast_to_llvm',['../translate_8h.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c'],['../translate_8c.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c']]]
 ];
--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -3,6 +3,7 @@ var searchData=
   ['parse_5fargs_0',['parse_args',['../arguments_8h.html#a4d7de48bbb1fd1e0943934cd0097f308',1,'parse_args(PurpleArgs *args, int argc, char *argv[]):&#160;arguments.c'],['../arguments_8c.html#a4d7de48bbb1fd1e0943934cd0097f308',1,'parse_args(PurpleArgs *args, int argc, char *argv[]):&#160;arguments.c']]],
   ['parse_5fbinary_5fexpression_1',['parse_binary_expression',['../parse_8h.html#a5289c1f11df62361e6a30e7bec3309c8',1,'parse_binary_expression(int previous_token_precedence):&#160;expression.c'],['../expression_8c.html#a5289c1f11df62361e6a30e7bec3309c8',1,'parse_binary_expression(int previous_token_precedence):&#160;expression.c']]],
   ['parse_5fopt_2',['parse_opt',['../arguments_8c.html#a5fb6abc38ef248ec154591e0dd392c1b',1,'arguments.c']]],
-  ['purple_5flog_3',['purple_log',['../logging_8h.html#a9cfd6399b1995d4e620bcb1aa53f4764',1,'purple_log(LogLevel level, const char *fmt,...):&#160;logging.c'],['../logging_8c.html#a9cfd6399b1995d4e620bcb1aa53f4764',1,'purple_log(LogLevel level, const char *fmt,...):&#160;logging.c']]],
-  ['put_5fback_5finto_5fstream_4',['put_back_into_stream',['../scan_8c.html#aa675e2a79ca95a2b6861a587655eef81',1,'scan.c']]]
+  ['prepend_5floaded_3',['prepend_loaded',['../llvm_8c.html#ac977d1d6aef44e3b2087f9b231723342',1,'llvm.c']]],
+  ['purple_5flog_4',['purple_log',['../logging_8h.html#a9cfd6399b1995d4e620bcb1aa53f4764',1,'purple_log(LogLevel level, const char *fmt,...):&#160;logging.c'],['../logging_8c.html#a9cfd6399b1995d4e620bcb1aa53f4764',1,'purple_log(LogLevel level, const char *fmt,...):&#160;logging.c']]],
+  ['put_5fback_5finto_5fstream_5',['put_back_into_stream',['../scan_8c.html#aa675e2a79ca95a2b6861a587655eef81',1,'scan.c']]]
 ];
