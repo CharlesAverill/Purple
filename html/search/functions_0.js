55ec5dd46bf180b45fbbb84ab02efe793b8ab503
Charles Averill <charlesaverill20@gmail.com>
2022-09-15 18:41:17 +0000

    Latest docs - 2022-09-10T00:42:04Z

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['add_5ffree_5fvirtual_5fregister_0',['add_free_virtual_register',['../translate_8h.html#aecdee1492308b67f9b70d84648d602e4',1,'add_free_virtual_register(type_register register_index):&#160;translate.c'],['../translate_8c.html#aecdee1492308b67f9b70d84648d602e4',1,'add_free_virtual_register(type_register register_index):&#160;translate.c']]],
-  ['ast_5fto_5fllvm_1',['ast_to_llvm',['../translate_8h.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c'],['../translate_8c.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c']]]
+  ['ast_5fto_5fllvm_0',['ast_to_llvm',['../translate_8h.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c'],['../translate_8c.html#ab4bb4871076aa73ec76e957aececfa8f',1,'ast_to_llvm(ASTNode *n):&#160;translate.c']]]
 ];
