6e29d26ed1de934dedd61d13bd6b9a9161f4e3ea
Charles Averill <charlesaverill20@gmail.com>
2022-09-28 16:38:33 +0000

    Latest docs - 2022-09-10T00:42:04Z

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -10,7 +10,7 @@ var searchData=
   ['generatorprogramwritten_7',['generatorProgramWritten',['../clang_8h.html#ab585cd02953166aee6f90d6d306e6eda',1,'clang.h']]],
   ['get_5fnext_5flabel_8',['get_next_label',['../llvm_8h.html#af6c6071ec265809fb6669298c71f6b09',1,'get_next_label(void):&#160;llvm.c'],['../llvm_8c.html#af6c6071ec265809fb6669298c71f6b09',1,'get_next_label(void):&#160;llvm.c']]],
   ['get_5fnext_5flocal_5fvirtual_5fregister_9',['get_next_local_virtual_register',['../llvm_8h.html#acf6ffab57a0d67bd99dc3caa06a9e1d6',1,'get_next_local_virtual_register(void):&#160;llvm.c'],['../llvm_8c.html#acf6ffab57a0d67bd99dc3caa06a9e1d6',1,'get_next_local_virtual_register(void):&#160;llvm.c']]],
-  ['get_5foperatorprecedence_10',['get_operatorPrecedence',['../expression_8c.html#ace021c537d1a5bacc6190333c1b5dcd4',1,'expression.c']]],
+  ['get_5foperator_5fprecedence_10',['get_operator_precedence',['../expression_8c.html#a3894f429297d723b0874c729e033242b',1,'expression.c']]],
   ['get_5fpostamble_11',['get_postamble',['../clang_8h.html#a05c1a47a02bdbc3937ea103b8a6eae63',1,'clang.h']]],
   ['get_5ftarget_5fdatalayout_12',['get_target_datalayout',['../clang_8h.html#aad50747c930d7d8fe05d741b81501f5c',1,'get_target_datalayout():&#160;clang.c'],['../clang_8c.html#aad50747c930d7d8fe05d741b81501f5c',1,'get_target_datalayout():&#160;clang.c']]],
   ['get_5ftarget_5ftriple_13',['get_target_triple',['../clang_8h.html#ae6b73131bb54576584f6bdb9dc6378a7',1,'get_target_triple():&#160;clang.c'],['../clang_8c.html#ae6b73131bb54576584f6bdb9dc6378a7',1,'get_target_triple():&#160;clang.c']]],
--- search/functions_4.js
+++ search/functions_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['generate_5fllvm_0',['generate_llvm',['../translate_8h.html#ac2560f9333db7b4bc4b0dd4ef15b3dcc',1,'generate_llvm(void):&#160;translate.c'],['../translate_8c.html#ac2560f9333db7b4bc4b0dd4ef15b3dcc',1,'generate_llvm(void):&#160;translate.c']]],
   ['get_5fnext_5flabel_1',['get_next_label',['../llvm_8h.html#af6c6071ec265809fb6669298c71f6b09',1,'get_next_label(void):&#160;llvm.c'],['../llvm_8c.html#af6c6071ec265809fb6669298c71f6b09',1,'get_next_label(void):&#160;llvm.c']]],
   ['get_5fnext_5flocal_5fvirtual_5fregister_2',['get_next_local_virtual_register',['../llvm_8h.html#acf6ffab57a0d67bd99dc3caa06a9e1d6',1,'get_next_local_virtual_register(void):&#160;llvm.c'],['../llvm_8c.html#acf6ffab57a0d67bd99dc3caa06a9e1d6',1,'get_next_local_virtual_register(void):&#160;llvm.c']]],
-  ['get_5foperatorprecedence_3',['get_operatorPrecedence',['../expression_8c.html#ace021c537d1a5bacc6190333c1b5dcd4',1,'expression.c']]],
+  ['get_5foperator_5fprecedence_3',['get_operator_precedence',['../expression_8c.html#a3894f429297d723b0874c729e033242b',1,'expression.c']]],
   ['get_5fpostamble_4',['get_postamble',['../clang_8h.html#a05c1a47a02bdbc3937ea103b8a6eae63',1,'clang.h']]],
   ['get_5ftarget_5fdatalayout_5',['get_target_datalayout',['../clang_8h.html#aad50747c930d7d8fe05d741b81501f5c',1,'get_target_datalayout():&#160;clang.c'],['../clang_8c.html#aad50747c930d7d8fe05d741b81501f5c',1,'get_target_datalayout():&#160;clang.c']]],
   ['get_5ftarget_5ftriple_6',['get_target_triple',['../clang_8h.html#ae6b73131bb54576584f6bdb9dc6378a7',1,'get_target_triple():&#160;clang.c'],['../clang_8c.html#ae6b73131bb54576584f6bdb9dc6378a7',1,'get_target_triple():&#160;clang.c']]],
