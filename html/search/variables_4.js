b236ed4c651a4988282bdbfde579c2d1aa36981b
Charles Averill <charlesaverill20@gmail.com>
2022-10-02 12:46:48 +0000

    Latest docs - 2022-09-10T00:42:04Z

--- html/search/variables_4.js
+++ html/search/variables_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['filename_0',['filename',['../structposition.html#a9ec1dda88ec8d157b7e8d9e52235f031',1,'position::filename()'],['../structASTNode.html#aa794bc0db32b5488f7e89e0b140a5676',1,'ASTNode::filename()']]],
   ['filenames_1',['filenames',['../structPurpleArgs.html#a472dbb5a4f9205da6be8af0239e38272',1,'PurpleArgs']]],
-  ['freevirtualregistershead_2',['freeVirtualRegistersHead',['../llvm_8h.html#a14931181b0e852af3c22f3d9f3678e48',1,'freeVirtualRegistersHead():&#160;translate.c'],['../translate_8c.html#a14931181b0e852af3c22f3d9f3678e48',1,'freeVirtualRegistersHead():&#160;translate.c']]]
+  ['freevirtualregistershead_2',['freeVirtualRegistersHead',['../llvm_8h.html#a14931181b0e852af3c22f3d9f3678e48',1,'freeVirtualRegistersHead():&#160;translate.c'],['../translate_8c.html#a14931181b0e852af3c22f3d9f3678e48',1,'freeVirtualRegistersHead():&#160;translate.c']]],
+  ['from_5fcommand_5fline_5fargument_3',['from_command_line_argument',['../structPurpleArgs.html#a806d1e0611813b1a173887916062d737',1,'PurpleArgs']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -4,7 +4,7 @@ var searchData=
   ['filename_1',['filename',['../structposition.html#a9ec1dda88ec8d157b7e8d9e52235f031',1,'position::filename()'],['../structASTNode.html#aa794bc0db32b5488f7e89e0b140a5676',1,'ASTNode::filename()']]],
   ['filenames_2',['filenames',['../structPurpleArgs.html#a472dbb5a4f9205da6be8af0239e38272',1,'PurpleArgs']]],
   ['find_5fsymbol_5ftable_5fentry_3',['find_symbol_table_entry',['../symbol__table_8h.html#a8cee987c629bab7017e0eea921e4ab1f',1,'find_symbol_table_entry(SymbolTable *table, char *symbol_name):&#160;symbol_table.c'],['../symbol__table_8c.html#a8cee987c629bab7017e0eea921e4ab1f',1,'find_symbol_table_entry(SymbolTable *table, char *symbol_name):&#160;symbol_table.c']]],
-  ['fnv_5f1_4',['FNV_1',['../hash_8h.html#ab17e65bc44528a6dded8ef5b965cdcff',1,'FNV_1(char *str):&#160;hash.c'],['../hash_8c.html#ab17e65bc44528a6dded8ef5b965cdcff',1,'FNV_1(char *str):&#160;hash.c']]],
+  ['fnv_5f1_4',['FNV_1',['../hash_8c.html#ab17e65bc44528a6dded8ef5b965cdcff',1,'FNV_1(char *str):&#160;hash.c'],['../hash_8h.html#ab17e65bc44528a6dded8ef5b965cdcff',1,'FNV_1(char *str):&#160;hash.c']]],
   ['fnv_5foffset_5fbasis_5',['FNV_OFFSET_BASIS',['../hash_8h.html#ad2026578664bf796de1faaa6d562ff17',1,'hash.h']]],
   ['fnv_5fprime_6',['FNV_PRIME',['../hash_8h.html#ad24caffb4ec2b57caeb823da822d3120',1,'hash.h']]],
   ['for_5fstatement_7',['for_statement',['../statement_8c.html#ac24b43c70d7c71aac2b50ab622019ec5',1,'statement.c']]],
@@ -12,5 +12,6 @@ var searchData=
   ['free_5fllvm_5fstack_5fentry_5fnode_5flist_9',['free_llvm_stack_entry_node_list',['../llvm__stack__entry_8h.html#a24f6f1959edad4155dc0418b5f041948',1,'free_llvm_stack_entry_node_list(LLVMStackEntryNode *head):&#160;llvm_stack_entry.c'],['../llvm__stack__entry_8c.html#a24f6f1959edad4155dc0418b5f041948',1,'free_llvm_stack_entry_node_list(LLVMStackEntryNode *head):&#160;llvm_stack_entry.c']]],
   ['free_5fsymbol_5ftable_5fstack_10',['free_symbol_table_stack',['../symbol__table_8h.html#a346fc23f4e7fa1a296eac45b999f6536',1,'free_symbol_table_stack(SymbolTableStack *stack):&#160;symbol_table.c'],['../symbol__table_8c.html#a346fc23f4e7fa1a296eac45b999f6536',1,'free_symbol_table_stack(SymbolTableStack *stack):&#160;symbol_table.c']]],
   ['freevirtualregistershead_11',['freeVirtualRegistersHead',['../llvm_8h.html#a14931181b0e852af3c22f3d9f3678e48',1,'freeVirtualRegistersHead():&#160;translate.c'],['../translate_8c.html#a14931181b0e852af3c22f3d9f3678e48',1,'freeVirtualRegistersHead():&#160;translate.c']]],
-  ['function_5fdeclaration_12',['function_declaration',['../parse_8h.html#a144fb627a89093eec468cacb91eacd3d',1,'function_declaration(void):&#160;declaration.c'],['../declaration_8c.html#a144fb627a89093eec468cacb91eacd3d',1,'function_declaration(void):&#160;declaration.c']]]
+  ['from_5fcommand_5fline_5fargument_12',['from_command_line_argument',['../structPurpleArgs.html#a806d1e0611813b1a173887916062d737',1,'PurpleArgs']]],
+  ['function_5fdeclaration_13',['function_declaration',['../parse_8h.html#a144fb627a89093eec468cacb91eacd3d',1,'function_declaration(void):&#160;declaration.c'],['../declaration_8c.html#a144fb627a89093eec468cacb91eacd3d',1,'function_declaration(void):&#160;declaration.c']]]
 ];
